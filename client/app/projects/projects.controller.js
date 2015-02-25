e8b7ee48f87f194b353ec7bea02c52514013ebd6
Aaron Gunderson <aaron@agundy.com>
2015-02-24 23:15:32 -0500

    Added very basic project page implementation with unordered list of projects

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('observatory3App')
-  .controller('ProjectsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ProjectsCtrl', function ($scope, $http) {
+    $scope.projects = [];
+
+    $http.get('/api/projects').success(function(projects) {
+      $scope.projects = projects;
+    });
   });
