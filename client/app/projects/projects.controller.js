b11bcbf99998d6d2280c45eb11fc69c7743336be
jfucci <jacfu777@gmail.com>
2015-03-12 17:29:31 -0400

    Cleaned up the form code a bit. The page now auto-updates when adding a project. Might want to seperate the controllers again in the future.

--- client/app/projects/projects.controller.js
+++ client/app/projects/projects.controller.js
@@ -1,25 +1,29 @@
 'use strict';
 
 angular.module('observatory3App')
-  .controller('ProjectsCtrl', function ($scope, $http) {
+.controller('ProjectsCtrl', function ($scope, $http) {
     $scope.projects = [];
 
     $http.get('/api/projects').success(function(projects) {
-      $scope.projects = projects;
+        $scope.projects = projects;
     });
-  });
 
-angular.module('observatory3App')
-  .controller('addCtrl', function ($scope, $http) {
-    $scope.project = {};
+    $scope.projectToAdd = {};
 
-    $scope.submit = function() {
+    $scope.submit = function(form) {
         $('#addProject').modal('hide');
+        if(form) {
+            form.$setPristine();
+            form.$setUntouched();
+        }
         // use setTimeout because hiding the modal takes longer than the post request
         // and results in the modal disappearing but the overlay staying if not used
         setTimeout(function() {
-            $http.post('/api/projects', $scope.project);
-            $scope.project = {};
+            $http.post('/api/projects', $scope.projectToAdd);
+            $scope.projectToAdd = {};
+            $http.get('/api/projects').success(function(projects) {
+                $scope.projects = projects;
+            });
         }, 200);
     }
 });
