383d63482a5525c096750c3f1f48c52954d87bff
kmcnellis <kiki15@me.com>
2015-03-19 19:45:31 -0400

    Change date to days in show user commits api

--- server/api/commit/index.js
+++ server/api/commit/index.js
@@ -9,6 +9,6 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.get('/project/:projectId', controller.showProjectCommits);
 router.get('/user/:githubProfile', controller.showUserCommits);
-router.get('/user/:githubProfile/date/:timeperiod', controller.showUserCommits);
+router.get('/user/:githubProfile/days/:timeperiod', controller.showUserCommits);
 
 module.exports = router;
