a3606f2aa7958f57d73b15939797c8f8c9f6ce83
kmcnellis <kiki15@me.com>
2015-04-07 20:11:49 -0400

    Fix styling of project page

--- client/app/projects/profile/profile.controller.js
+++ client/app/projects/profile/profile.controller.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('observatory3App')
-.controller('ProjectsProfileCtrl', function ($scope, $http, $stateParams) {
-    $http.get('/api/projects/'+ $stateParams.username + '/' + $stateParams.project).success(function(project){
-        $scope.project = project;
-    });
+.controller('ProjectsProfileCtrl', function ($scope, $http, Auth, $stateParams) {
+  $scope.isAuthor = false;
+  $scope.isLoggedIn = Auth.isLoggedIn;
+  $scope.isAdmin = Auth.isAdmin;
+  $scope.getCurrentUser = Auth.getCurrentUser();
 
+  $http.get('/api/projects/'+ $stateParams.username + '/' + $stateParams.project).success(function(project){
+    $scope.project = project;
 
+    if ($scope.isLoggedIn()){
+      for(var i = 0 ; i < $scope.project.authors.length ; i++){
+        if($scope.project.authors[i]._id == $scope.getCurrentUser._id){
+          $scope.isAuthor = true;
+        }
+      }
+    }
+  });
 });
