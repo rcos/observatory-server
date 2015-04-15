4804acd901d81007d30f3a3172b35986da2a00d4
Nathan Bernard <natedawg1013@gmail.com>
2015-04-14 22:29:49 -0400

    Working file uploads. Limited to 10/project, no type checking yet, needs better front end interface.

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('observatory3App', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'angularFileUpload'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
--- server/api/project/index.js
+++ server/api/project/index.js
@@ -9,6 +9,7 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/past', controller.indexOld);
 router.get('/:username/:project', controller.show);
+router.post('/:username/:project/upload', controller.upload);
 router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
