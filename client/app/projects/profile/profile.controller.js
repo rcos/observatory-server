ae0674a196e611001fc3b37c25ba03cf92a2e2ff
jfucci <jacfu777@gmail.com>
2015-04-28 21:32:27 -0400

    Added a photo carousel and new image upload button to the project profile page.

--- client/app/projects/profile/profile.controller.js
+++ client/app/projects/profile/profile.controller.js
@@ -1,27 +1,29 @@
 'use strict';
 
 angular.module('observatory3App')
-.controller('ProjectsProfileCtrl', function ($scope, $http, $stateParams, $upload) {
+.controller('ProjectsProfileCtrl', function ($scope, $http, $stateParams, $upload, Auth) {
     $http.get('/api/projects/'+ $stateParams.username + '/' + $stateParams.project).success(function(project){
       $scope.project = project;
     });
 
     $scope.imgPrefix = "/uploads/" + $stateParams.username + '/' + $stateParams.project + '/';
 
-    $scope.onFileSelect = function($files) {
-    console.log("file chosen");
-    //$files: an array of files selected, each file has name, size, and type.
-    for (var i = 0; i < $files.length; i++) {
-      var $file = $files[i];
-      $upload.upload({
-        url: 'api/projects/'+$stateParams.username+'/'+$stateParams.project+'/upload',
-        file: $file,
-        progress: function(e){}
-      }).then(function(data, status, headers, config) {
-        // file is uploaded successfully
-        console.log(data);
-      }); 
+    $scope.userInProject = function() {
+        return true; // delete when we get people in projects
+        return $scope.project.authors.indexOf(Auth.getCurrentUser()._id) !== -1;
     }
-  }
 
+    $scope.onFileSelect = function($files) {
+        //$files: an array of files selected, each file has name, size, and type.
+        for (var i = 0; i < $files.length; i++) {
+            var $file = $files[i];
+            $upload.upload({
+                url: 'api/projects/'+$stateParams.username+'/'+$stateParams.project+'/upload',
+                file: $file
+            });
+        }
+        $http.get('/api/projects/'+ $stateParams.username + '/' + $stateParams.project).success(function(project){
+            $scope.project = project;
+        });
+    }
 });
