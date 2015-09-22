671da64954d6f0eee25d26de4e49f21a0362203b
Zaran <zlalvani@gmail.com>
2015-09-22 17:07:20 -0400

    fixes for bash script

--- vagrant.sh
+++ vagrant.sh
@@ -2,6 +2,9 @@
 
 echo "Provisioning virtual machine..."
 
+echo "Installing git"
+apt-get install -y git
+
 echo "Installing NodeJS"
 apt-get update 
 apt-get install -y ruby-full build-essential
@@ -10,24 +13,22 @@ apt-get install -y nodejs-legacy
 echo "Installing npm"
 apt-get install -y npm
 
-echo "Installing MonogDB"
+echo "Installing MongoDB"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" |  tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
-apt-get update
+#apt-get update
 
 apt-get install -y mongodb-org
-
-npm install grunt
  
-#chown $USER -R ~./npm
-sudo npm install -g grunt-cli grunt bower
-npm install
-bower install
-sudo gem install sass 
-sudo service mongodb start
-#sudo mkdir -p /data/db
 cd /vagrant
-#grunt serve
+chown $USER -R ~/.npm
+sudo npm install -g grunt-cli grunt bower
+sudo npm install
+sudo bower install --allow-root
+sudo gem install sass 
+sudo mkdir -p /data/db
+sudo service mongod start
+grunt serve
 
