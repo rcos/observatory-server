bdded20d648bb0e2aa53d8bfeea45838e10e27be
Kiana McNellis <kiana@kmcnellis.com>
2015-09-27 14:52:07 -0400

    add auth on semester

--- server/api/classyear/index.js
+++ server/api/classyear/index.js
@@ -2,17 +2,19 @@
 
 var express = require('express');
 var controller = require('./classyear.controller');
+var config = require('../../config/environment');
 var auth = require('../../auth/auth.service');
+
 var router = express.Router();
 
 // TODO permissions
 router.get('/', controller.index);
 router.get('/displayURP', controller.getDisplayURP);
-router.get('/semester/:semester', controller.getClassYear);
-router.post('/', controller.create);
-router.post('/daycode', controller.daycode);
-router.put('/displayURP', controller.displayURP);
-router.put('/semester/:semester', controller.update);
+router.get('/semester/:semester',  auth.hasRole('admin'), controller.getClassYear);
+router.post('/',  auth.hasRole('admin'), controller.create);
+router.post('/daycode', auth.hasRole('admin'), controller.daycode);
+router.put('/displayURP', auth.hasRole('admin'), controller.displayURP);
+router.put('/semester/:semester', auth.hasRole('admin'), controller.update);
 router.delete('/semester/:semester', auth.hasRole('admin') ,controller.destroy);
 
 module.exports = router;
