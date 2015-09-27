ae91d55096c0d05526210904614ec8b64696d4af
Kiana McNellis <kiana@kmcnellis.com>
2015-09-27 18:09:23 -0400

    Switch to 4 space tabs

--- client/app/projects/profile/profile.controller.js
+++ client/app/projects/profile/profile.controller.js
@@ -1,200 +1,200 @@
 /*jshint multistr: true */
-'use strict';
+'use strict';  
 
 angular.module('observatory3App')
 .controller('ProjectsProfileCtrl', function ($scope, $http, Auth, $stateParams, Upload, Project) {
-  $scope.userOnProject = false;
-  var updateProject = function(){
-    Project.getProject($stateParams.username, $stateParams.project).then(function(result) {
-      $scope.project = result.data;
-      initializeSlides($scope.project.photos);
-      getAuthors();
-      Auth.isLoggedInAsync(function(loggedIn){
-        if (loggedIn){
-          var user = Auth.getCurrentUser();
-          $scope.user = user;
-          $scope.checkUserProject();
+    $scope.userOnProject = false;
+    var updateProject = function(){
+        Project.getProject($stateParams.username, $stateParams.project).then(function(result) {
+            $scope.project = result.data;
+            initializeSlides($scope.project.photos);
+            getAuthors();
+            Auth.isLoggedInAsync(function(loggedIn){
+                if (loggedIn){
+                    var user = Auth.getCurrentUser();
+                    $scope.user = user;
+                    $scope.checkUserProject();
+                }
+            });
+        });
+    };
+    updateProject();   
+
+    var getAuthors = function() {
+        var project = $scope.project;
+        $http.get('/api/projects/' + project._id + '/authors')
+        .success(function(authors){
+            $scope.authors = authors;
+        });
+    }; 
+
+    $scope.getPic = function(user) {   
+
+        if (! ('avatar' in user)){
+            user.avatar = "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=monsterid";
+            $http.get('/api/users/' + user._id + '/avatar')            .success(function(avatar){
+                user.avatar = avatar;
+            })
         }
-      });
-    });
-  };
-  updateProject();
+        return user.avatar
+    }  
 
-  var getAuthors = function() {
-    var project = $scope.project;
-    $http.get('/api/projects/' + project._id + '/authors')
-    .success(function(authors){
-      $scope.authors = authors;
-    });
-  };
+    var initializeSlides = function(photos) {
+        var slides = [];
+        for (var i = 0; i < photos.length; i++){
+            slides.push({
+                active: false,
+                src: photos[i]
+            });
+            if (i === 0) {
+                slides[0].active = true;
+            }
+        }
+        $scope.slides = slides;
+    }  
 
-  $scope.getPic = function(user) {
+    var setActiveSlide = function(photoName){
+        for (var i = 0; i < $scope.slides.length; i++){
+            if ($scope.slides[i].src === photoName){
+                $scope.slides[i].active = true;
+            } else {
+                $scope.slides[i].active = false;
+            }
+        }
+    };
+    updateProject();   
 
-    if (! ('avatar' in user)){
-      user.avatar = "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=monsterid";
-      $http.get('/api/users/' + user._id + '/avatar')            .success(function(avatar){
-        user.avatar = avatar;
-      })
-    }
-    return user.avatar
-  }
+    var getAuthors = function() {
+        var project = $scope.project;
+        $http.get('/api/projects/' + project._id + '/authors')
+        .success(function(authors){
+            $scope.authors = authors;
+        });
+    }; 
 
-  var initializeSlides = function(photos) {
-    var slides = [];
-    for (var i = 0; i < photos.length; i++){
-      slides.push({
-        active: false,
-        src: photos[i]
-      });
-      if (i === 0) {
-        slides[0].active = true;
-      }
-    }
-    $scope.slides = slides;
-  }
+    var initializeSlides = function(photos) {
+        var slides = [];
+        for (var i = 0; i < photos.length; i++){
+            slides.push({
+                active: false,
+                src: photos[i]
+            });
+            if (i === 0) {
+                slides[0].active = true;
+            }
+        }
+        $scope.slides = slides;
+    }; 
 
-  var setActiveSlide = function(photoName){
-    for (var i = 0; i < $scope.slides.length; i++){
-      if ($scope.slides[i].src === photoName){
-        $scope.slides[i].active = true;
-      } else {
-        $scope.slides[i].active = false;
-      }
-    }
-  };
-  updateProject();
+    var setActiveSlide = function(photoName){
+        for (var i = 0; i < $scope.slides.length; i++){
+            if ($scope.slides[i].src === photoName){
+                $scope.slides[i].active = true;
+            } else {
+                $scope.slides[i].active = false;
+            }
+        }
+    }; 
 
-  var getAuthors = function() {
-    var project = $scope.project;
-    $http.get('/api/projects/' + project._id + '/authors')
-    .success(function(authors){
-      $scope.authors = authors;
-    });
-  };
+    var addSlide = function(photoName){
+        $scope.slides.push({
+            active: false,
+            src: photoName
+        });
+        setActiveSlide(photoName);
+    }; 
 
-  var initializeSlides = function(photos) {
-    var slides = [];
-    for (var i = 0; i < photos.length; i++){
-      slides.push({
-        active: false,
-        src: photos[i]
-      });
-      if (i === 0) {
-        slides[0].active = true;
-      }
-    }
-    $scope.slides = slides;
-  };
+    var removeSlide = function(photoName){
+        for (var i = 0; i < $scope.slides.length; i++){
+            if ($scope.slides[i].src === photoName){
+                $scope.slides.splice(i, 1);
+            }
+        }
+    }; 
+    
 
-  var setActiveSlide = function(photoName){
-    for (var i = 0; i < $scope.slides.length; i++){
-      if ($scope.slides[i].src === photoName){
-        $scope.slides[i].active = true;
-      } else {
-        $scope.slides[i].active = false;
-      }
-    }
-  };
+    $scope.imgPrefix = '/uploads/' + $stateParams.username + '/' + $stateParams.project + '/'; 
+    
 
-  var addSlide = function(photoName){
-    $scope.slides.push({
-      active: false,
-      src: photoName
-    });
-    setActiveSlide(photoName);
-  };
-
-  var removeSlide = function(photoName){
-    for (var i = 0; i < $scope.slides.length; i++){
-      if ($scope.slides[i].src === photoName){
-        $scope.slides.splice(i, 1);
-      }
-    }
-  };
-
-
-  $scope.imgPrefix = '/uploads/' + $stateParams.username + '/' + $stateParams.project + '/';
-
-
-  $scope.edittingDesc = false;
-  $scope.isLoggedIn = Auth.isLoggedIn;
-
-  $scope.editDesc = function(){
-    $scope.edittingDesc = !$scope.edittingDesc;
-  };
-
-  // Function for saving the description
-  $scope.saveDesc = function(){
     $scope.edittingDesc = false;
-    $http.put('/api/projects/' + $scope.project._id, {
-      'description': $scope.project.description
-    }).success(function(){
-      window.alert('Description updated!');
-    }).error(function(){
-      window.alert('Could not update description!');
-    });
-  };
+    $scope.isLoggedIn = Auth.isLoggedIn;   
 
-  $scope.joinProject = function(){
-    $http.put('/api/users/' + $scope.user._id + '/project',{
-      'project': $scope.project._id
-    }).success(function(){
-      window.alert('You are now on this project!');
-      $scope.userOnProject = true;
-      $scope.user.projects.push($scope.project._id);
-      getAuthors();
-    }).error(function(){
-      window.alert('Error adding user to project!');
-    });
-  };
+    $scope.editDesc = function(){
+        $scope.edittingDesc = !$scope.edittingDesc;
+    }; 
 
-  $scope.leaveProject = function(){
-    var loggedInUser = Auth.getCurrentUser();
-    $http.delete('/api/users/' + loggedInUser._id + '/project',
-    {
-      'project': $scope.project._id
-    }).success(function(){
-      window.alert('You are now off this project!');
-      $scope.user.projects.splice($scope.user.projects.indexOf($scope.project._id), 1);
-      $scope.userOnProject = false;
-      getAuthors();
-    }).error(function(){
-      window.alert('Error removing user from project!');
-    });
-  };
+    // Function for saving the description
+    $scope.saveDesc = function(){
+        $scope.edittingDesc = false;
+        $http.put('/api/projects/' + $scope.project._id, {
+            'description': $scope.project.description
+        }).success(function(){
+            window.alert('Description updated!');
+        }).error(function(){
+            window.alert('Could not update description!');
+        });
+    }; 
 
+    $scope.joinProject = function(){
+        $http.put('/api/users/' + $scope.user._id + '/project',{
+            'project': $scope.project._id
+        }).success(function(){
+            window.alert('You are now on this project!');
+            $scope.userOnProject = true;
+            $scope.user.projects.push($scope.project._id);
+            getAuthors();
+        }).error(function(){
+            window.alert('Error adding user to project!');
+        });
+    }; 
 
-  $scope.checkUserProject = function() {
-    $scope.userOnProject = $scope.user.projects.indexOf($scope.project._id) !== -1;
-  };
+    $scope.leaveProject = function(){
+        var loggedInUser = Auth.getCurrentUser();
+        $http.delete('/api/users/' + loggedInUser._id + '/project',
+        {
+            'project': $scope.project._id
+        }).success(function(){
+            window.alert('You are now off this project!');
+            $scope.user.projects.splice($scope.user.projects.indexOf($scope.project._id), 1);
+            $scope.userOnProject = false;
+            getAuthors();
+        }).error(function(){
+            window.alert('Error removing user from project!');
+        });
+    }; 
+    
 
-  $scope.upload = function($file) {
-    Upload.upload({
-      url: 'api/projects/'+$stateParams.username+'/'+$stateParams.project+'/upload',
-      data: {file: $file}
-    }).success(function (data) {
-      addSlide(data);
-    }).error(function (data, status) {
-      console.log('error status: ' + status);
-    });
-  };
+    $scope.checkUserProject = function() {
+        $scope.userOnProject = $scope.user.projects.indexOf($scope.project._id) !== -1;
+    }; 
 
-  $scope.deletePhoto = function(){
-    var username = $scope.project.githubUsername;
-    var projectName = $scope.project.githubProjectName;
-    var activePhoto = $scope.slides.filter(function (s) { return s.active; })[0];
-    if (activePhoto){
-      $http.delete('/api/projects/' +  username + '/' + projectName + '/' + activePhoto.src)
-      .success(function(){
-        removeSlide(activePhoto.src);
-      });
-    }
-  };
+    $scope.upload = function($file) {
+        Upload.upload({
+            url: 'api/projects/'+$stateParams.username+'/'+$stateParams.project+'/upload',
+            data: {file: $file}
+        }).success(function (data) {
+            addSlide(data);
+        }).error(function (data, status) {
+            console.log('error status: ' + status);
+        });
+    }; 
+
+    $scope.deletePhoto = function(){
+        var username = $scope.project.githubUsername;
+        var projectName = $scope.project.githubProjectName;
+        var activePhoto = $scope.slides.filter(function (s) { return s.active; })[0];
+        if (activePhoto){
+            $http.delete('/api/projects/' +  username + '/' + projectName + '/' + activePhoto.src)
+            .success(function(){
+                removeSlide(activePhoto.src);
+            });
+        }
+    };
 })
 .directive('desc', function() {
-  return {
-    restrict:'E',
-    template: '<div btf-markdown=\'project.description\'></div> \
-    <textarea ng-show=\'edittingDesc && userOnProject\' ng-model=\'project.description\' ></textarea>'
-  };
+    return {
+        restrict:'E',
+        template: '<div btf-markdown=\'project.description\'></div> \
+        <textarea ng-show=\'edittingDesc && userOnProject\' ng-model=\'project.description\' ></textarea>'
+    };
 });
