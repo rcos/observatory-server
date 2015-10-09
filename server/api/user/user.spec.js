762b9a3e3ea57419b52045c728966b7ac06f7180
haoxinLuo <luoh2@rpi.edu>
2015-10-09 13:52:28 -0400

    edited 'api.endpoint'

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -8,7 +8,7 @@ describe('GET /api/users', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/users')
+      .get('/api/users/past')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
