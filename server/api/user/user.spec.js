e7084c94638e8b5b5aa6e39def36c36dbf9ffc84
Mike Dame <damemi@gmail.com>
2015-10-23 13:41:21 -0400

    /auth/createUser?

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,14 +4,14 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('POST /api/createUser', function() {
+describe('POST /auth/createUser', function() {
     it('should respond with JSON array', function(done) {
 	var postData = {
 	    "user":{}
 	};
 
 	request(app)
-	    .post('/api/createUser')
+	    .post('/auth/createUser')
 	    .send(postData)
 	    .expect(200)
 	    .end(function(err, res) {
