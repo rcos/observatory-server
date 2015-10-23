421781c32be63c31f8156f9790c325c62c854908
haoxinLuo <luoh2@rpi.edu>
2015-10-23 12:08:43 -0400

    travis ci test

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,6 +4,7 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
+
 describe('GET /api/users/past', function() {
 
   it('should respond with JSON array', function(done) {
