95df86fe237ac3ae6337dbb205aff1d9366bdf87
Severin Ibarluzea <seveibar@gmail.com>
2015-10-26 18:33:06 -0400

    removed index comments

--- server/api/smallgroup/index.js
+++ server/api/smallgroup/index.js
@@ -9,10 +9,6 @@ var router = express.Router();
 
 // TODO permissions
 router.get('/', controller.index);
-// router.post('/',  auth.hasRole('admin'), controller.create);
-
-// router.get('/displayURP', controller.getDisplayURP);
-// router.put('/displayURP', auth.hasRole('admin'), controller.displayURP);
 
 router.post('/', auth.hasRole('mentor'), controller.create);
 router.put('/:id', auth.hasRole('mentor'), controller.modify);
@@ -21,11 +17,5 @@ router.put('/:id/member', controller.addMember);
 router.get('/:id', auth.isAuthenticated(), controller.getSmallGroup);
 router.get('/:id', controller.getSmallGroup);
 router.delete('/:id', auth.hasRole('mentor'), controller.delete);
-
-// router.get('/semester/:semester',  auth.hasRole('admin'), controller.getClassYear);
-// router.put('/semester/:semester', auth.hasRole('admin'), controller.update);
-// router.delete('/semester/:semester', auth.hasRole('admin') ,controller.destroy);
-
-// router.post('/daycode', auth.hasRole('admin'), controller.daycode);
 
 module.exports = router;
