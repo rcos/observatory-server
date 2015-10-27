43049925122dd6b1ed8e7eeebfe8cc281c2786e6
Mike Dame <damemi@gmail.com>
2015-10-27 12:06:22 -0400

    Typo

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,14 +4,14 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('POST /api/user', function() {
+describe('POST /api/user/', function() {
     it('should respond with JSON array', function(done) {
 	var postData = {
 	    "user":{}
 	};
 
 	request(app)
-	    .post('/api/user')
+	    .post('/api/user/')
 	    .send(postData)
 	    .expect(200)
 	    .end(function(err, res) {
