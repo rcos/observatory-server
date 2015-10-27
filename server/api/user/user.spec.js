5bffca41bc01d1dcb09444ef1dad77153c120845
Mike Dame <damemi@gmail.com>
2015-10-27 11:56:19 -0400

    Changing url to /api/user

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,14 +4,14 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('POST /auth/createUser', function() {
+describe('POST /pi/user', function() {
     it('should respond with JSON array', function(done) {
 	var postData = {
 	    "user":{}
 	};
 
 	request(app)
-	    .post('/auth/createUser')
+	    .post('/api/user')
 	    .send(postData)
 	    .expect(200)
 	    .end(function(err, res) {
