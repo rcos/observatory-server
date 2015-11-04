0e92a05f49b55b48844a27931cef2c9389179817
Zaran <zlalvani@gmail.com>
2015-11-03 22:21:39 -0500

    added starter projects page

--- client/app/projectchooser/projectchooser.controller.js
+++ client/app/projectchooser/projectchooser.controller.js
@@ -1,29 +1,59 @@
 'use strict';
 
 angular.module('observatory3App')
-  .controller('ProjectChooserCtrl', function ($scope, $stateParams, $http, Auth, User, $location, notify) {
+  .controller('ProjectChooserCtrl', function ($scope, $stateParams, $http, Auth, User, $location, notify, Project) {
     $scope.defaultProjects = [];
+    $scope.isLoggedIn = Auth.isLoggedIn;
 
     $scope.getDefaultProjects = function() {
         $http.get('/api/projects/defaults').success(function(projects) {
             $scope.defaultProjects = projects;
-            /*
-            for(var project in projects) {
-                console.log("looking at project " + project._id);
-                console.log(project.markedDefault);
-                if(project.markedDefault) {
-                    $scope.defaultProjects.push(project);
-                    console.log("test");
-                }
-            }
-        */
+            console.log($scope.defaultProjects[0].photos);
+            updateProjects();
         });
     };
 
     $scope.getDefaultProjects();
+    
+    var updateProjects = function(){
+        Auth.isLoggedInAsync(function(loggedIn){
+            if (loggedIn){
+                var user = Auth.getCurrentUser();
+                $scope.user = user;
+            }
+        });
+    };  
+    
+    $scope.checkUserProject = function(project) {
+        return $scope.user.projects.indexOf(project._id) !== -1;
+    };
+
+    $scope.joinProject = function(project){
+        $http.put('/api/users/' + $scope.user._id + '/project',{
+            'project': project._id
+        }).success(function(){
+            notify({ message: "You are now on this project!"  });
+            $scope.user.projects.push(project._id);
+        }).error(function(){
+            notify({message: 'Error adding user to project!', classes: ["alert-danger"]});
+        });
+    };
+
+    $scope.leaveProject = function(project){
+        var loggedInUser = Auth.getCurrentUser();
+        $http.delete('/api/users/' + $scope.user._id + '/project',
+        {
+            'project': project._id
+        }).success(function(){
+            notify({message: "You are now off this project!", classes: []});
+            $scope.user.projects.splice($scope.user.projects.indexOf(project._id), 1);
+        }).error(function(){
+            notify({message: 'Error removing user from project!', classes: ["alert-danger"]});
+        });
+    };  
 
     $scope.getImagePrefix = function(project) {
-        return '/uploads/' + project.username + '/' + project.project + '/';
+        return '/uploads/' + project.githubUsername + '/' + project.githubProjectName + '/';
     };
 
   });
