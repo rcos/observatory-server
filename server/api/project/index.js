585dd0b61a6d9f660bb36d5f9d450c8d76db064b
Istyatur <alexanderslanski@sbcglobal.net>
2015-11-15 09:56:24 -0500

    anyone who is part of the projecet can now edit the tech tags

--- server/api/project/index.js
+++ server/api/project/index.js
@@ -20,6 +20,6 @@ router.put('/:id/unmarkdefault', auth.hasRole('admin'), controller.unmarkDefault
 
 router.delete('/:id', auth.hasRole('mentor'), controller.destroy);
 
-router.put('/addTechBubble/:id/:tech', auth.hasRole('mentor'), controller.addTechBubble);
-router.put('/:id/:tech/removeTech', auth.hasRole('mentor'), controller.removeTech);
+router.put('/addTechBubble/:id/:tech', auth.isAuthenticated(), controller.addTechBubble);
+router.put('/:id/:tech/removeTech', auth.isAuthenticated(), controller.removeTech);
 module.exports = router;
