fdc19db4fd216639095f3b9b9c74ffa79969b0c5
Kiana McNellis <kiana@kmcnellis.com>
2016-04-06 17:01:34 -0400

    Add modal

---
+++ client/app/account/settings/confirmDelete/confirmDelete.directive.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('observatory3App')
+.controller('deleteUserController', function($scope, $location, $http, $uibModalInstance, Auth, notify){
+  $scope.submit = function(form) {
+    form.password.$setValidity('mongoose', true);
+
+    if(form.$valid) {
+      $scope.dismiss = false;
+      Auth.deleteUser($scope.password)
+      .then(function(response){
+        $scope.submitted = false;
+        notify({message: "Account deleted"});
+        $uibModalInstance.close(response);
+
+      },function(){
+          $scope.submitted = true;
+
+          $scope.errors = {};
+
+          form.password.$setValidity('mongoose', false);
+          $scope.errors.password = 'Incorrect password';
+      });
+
+    }
+  };
+  $scope.close = function(){
+    // $uibModalInstance.close($scope.event);
+    $uibModalInstance.dismiss('cancel');
+  };
+
+});
