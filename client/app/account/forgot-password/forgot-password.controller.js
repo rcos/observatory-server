50c7b301b480fe6b7e32ae7ffdbd788c1fdb1bc7
Aaron Gunderson <aaron@agundy.com>
2016-04-19 17:45:45 -0400

    Handle no sendgrid api key defined

--- client/app/account/forgot-password/forgot-password.controller.js
+++ client/app/account/forgot-password/forgot-password.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('observatory3App')
-.controller('ForgotPasswordCtrl', function ($scope, Auth) {
+.controller('ForgotPasswordCtrl', function ($scope, Auth, notify) {
   $scope.user = {};
   $scope.errors = {};
 
@@ -9,13 +9,14 @@ angular.module('observatory3App')
     $scope.submitted = true;
 
     if(form.$valid) {
-      Auth.resetPassword($scope.user.email)
-        .then( function(){
+      Auth.resetPassword($scope.user.email, function(err, res){
+        if (!err){
           $scope.success = true;
-        }).catch(function(err) {
-          $scope.errors.other = err;
-        });
+        }
+        else {
+          notify({ message: "Error: Problem Sending Password Reset Email", classes: ["alert-danger"] });
+        }
+      });
     }
   };
-
 });
