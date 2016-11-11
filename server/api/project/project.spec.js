1722dfe043b88ec30ae2b571e1d1e99daeb5d707
vkr96 <varunkrishrao@yahoo.com>
2016-11-11 12:52:15 -0500

    Project tests (#535)
    * Added some new tests
    
    * Added tests that check if project creation and deletion work

--- server/api/project/project.spec.js
+++ server/api/project/project.spec.js
@@ -1,11 +1,13 @@
 'use strict';
+import {seed} from '../../config/seed';
+import {getSessionFor} from '../../auth/util.integration';
+import Project from '../project/project.model';
 
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 var agent = request.agent(app);
 var project = request.agent();
-var studentSession;
 
 describe('GET /api/projects', function() {
 
@@ -38,6 +40,12 @@ describe('GET /api/projects/past', function() {
 });
 
 describe('POST /api/projects/', function() {
+  before(seed);
+  var studentSession;
+  var adminSession;
+  before(() => getSessionFor('student').then(session => studentSession = session));
+  before(() => getSessionFor('admin').then(session => adminSession = session));
+
   it('Should create a session', function(done) {
     agent.post('/api/users')
     .send({ username: 'a@yahoo.com', password: '123' })
@@ -54,4 +62,29 @@ describe('POST /api/projects/', function() {
       done();
     });
   });
+
+  it('Should create a project',done => {
+    studentSession
+      .post('/api/projects/')
+      .send({
+        title: "Test Project",
+        description: "Testing",
+        websiteUrl: "www.google.com"
+      })
+      .end((err,res) => {
+        expect(res.status).to.equal(201);
+        done(err);
+     });
+  });
+
+  it('Should delete a project', done=> {
+    var test_id = "000000000000000000000010";
+    adminSession
+      .delete('/api/projects/'+test_id)
+      .end((err,res) => {
+        expect(res.status).to.equal(204);
+        done(err);
+     });
+  });
+
 });
