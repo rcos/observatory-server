642e79854653577ba42efddd9b2a52616866f224
Jia Yong Zeng <fmossaic@gmail.com>
2016-12-06 03:31:39 -0500

    add test for #213

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -1,4 +1,6 @@
 'use strict';
+import {seed} from '../../config/seed';
+import {getSessionFor} from '../../auth/util.integration';
 
 var should = require('should');
 var app = require('../../app');
@@ -17,4 +19,36 @@ describe('GET /api/users/past', function() {
         done();
       });
   });
+});
+
+describe('getting another user as student', function() {
+  before(seed);
+  var studentSession;
+  before(() => getSessionFor('student').then(session => studentSession = session));
+
+  it('should not allow student to view the other\'s private info', done => {
+    var userId = "000000000000000000000400";
+
+    studentSession
+      .get('/api/users/' + userId + '/private')
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        done(err);
+      });
+  });
+
+  it('public profile should not contain private info ', done => {
+    var userId = "000000000000000000000400";
+
+    studentSession
+      .get('/api/users/' + userId)
+      .end((err, res) => {
+        if(res.body.attendance){
+          console.log("should not have access other/'s attudence")
+          done();
+        }
+        expect(res.status).to.equal(200);
+        done();
+      });
+  }); 
 });
