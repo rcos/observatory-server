78be8d5aa392351ba1cd9ec592f10cccc74e5a68
vkr96 <varunkrishrao@gmail.com>
2016-12-09 16:47:27 -0500

    Added test that makes sure mentor can access attendacne json file

--- server/api/attendance/attendance.spec.js
+++ server/api/attendance/attendance.spec.js
@@ -1,4 +1,7 @@
 'use strict';
+import {seed} from '../../config/seed';
+import {getSessionFor} from '../../auth/util.integration';
+import Attendance from '../attendance/attendance.model';
 
 var should = require('should');
 var app = require('../../app');
@@ -17,4 +20,21 @@ describe('GET /api/commits', function() {
         done();
       });
   });
+});
+
+describe('GET /api/attendance', function() {
+  before(seed);
+  var session,mentorSession;
+  before(() => getSessionFor('mentor').then(session => mentorSession = session));
+
+    it('should respond with list of attendance submissions',done=>{
+        mentorSession
+        .get('/api/attendance')
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          res.body.should.be.instanceof(Array);
+          done(err);
+        });
+    });
+
 });
