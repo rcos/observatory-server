c1922428f3f63b6d18e7f7fc8016e306227c57cf
Michael Zemsky <zmike808@gmail.com>
2017-03-09 12:04:23 -0500

    POST /api/users API tests (#622)
    * removed a user api test from project spec; user API tests should be in user specs...
    
    * changed HTTP status code for user creation from 200 to 201
    
    * Added user creation API tests
    
    * fixed incorrect status code from 201 to 422
    
    * disabled invalid email test until input validation is implemented
    
    * added new package (fakerator) to package.json

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -5,15 +5,16 @@ import {getSessionFor} from '../../auth/util.integration';
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var faker = require("fakerator")();
 
-describe('GET /api/users/past', function() {
+describe('GET /api/users/past', function () {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with JSON array', function (done) {
     request(app)
       .get('/api/users/past')
       .expect(200)
       .expect('Content-Type', /json/)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
         done();
@@ -21,7 +22,7 @@ describe('GET /api/users/past', function() {
   });
 });
 
-describe('getting another user as student', function() {
+describe('getting another user as student', function () {
   before(seed);
   var studentSession;
   before(() => getSessionFor('student').then(session => studentSession = session));
@@ -46,6 +47,58 @@ describe('getting another user as student', function() {
         should.not.exist(res.body.attendance);
         expect(res.status).to.equal(200);
         done();
+      });
+  });
+});
+
+describe('POST /api/users/', function () {
+  before(seed);
+
+  it('Should create a user', done => {
+    request(app)
+      .post('/api/users/')
+      .send({
+        "name": faker.names.name(),
+        "email": faker.internet.email(),
+        "github": {"login": faker.internet.userName()},
+        "password": "test"
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        done(err);
+      });
+  });
+  /**
+   * @TODO: DISABLED UNTIL INPUT VALIDATION IS IMPLEMENTED
+   **/
+  xit('Attempt to create a user with invalid email', done => {
+    var p = {
+      "name": faker.names.name(),
+      "email": faker.internet.userName(),
+      "github": {"login": faker.internet.userName()},
+      "password": "test"
+    };
+    request(app)
+      .post('/api/users/')
+      .send(p)
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        done(err);
+      });
+  });
+
+  it('Attempt to create a user with no password', done => {
+    request(app)
+      .post('/api/users/')
+      .send({
+        "name": faker.names.name(),
+        "email": faker.internet.email(),
+        "github": {"login": faker.internet.userName()},
+        "password": ""
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(422);
+        done(err);
       });
   });
 });
