8e71e921d4b739a4f7d1746da29ce5ccaed85e30
Andrew Mwakibinga <Andrew Mwakibinga>
2018-02-27 16:24:40 -0500

    Merged upstream

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,9 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/observatory-dev'
+    // uri: 'mongodb://localhost/observatory-dev'
+    // uri: 'mongodb://localhost/observatory3-dev'
+    uri: 'mongodb://192.168.99.100:27017/observatory3-dev'
   },
 
   // Server Address
