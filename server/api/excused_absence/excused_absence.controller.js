cc5a59552f2b41e6eef77fc4374dd8990eddf5a2
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-10 18:12:08 -0500

    Added notes & todos to excused_absence.controller.js

--- server/api/excused_absence/excused_absence.controller.js
+++ server/api/excused_absence/excused_absence.controller.js
@@ -21,7 +21,7 @@ exports.index = (req, res) => {
   .catch((err) => {
     return handleError(res, err)
   })
-};
+}
 
 /**
 * @api {get} /api/excused_absences/:id Show
@@ -47,7 +47,7 @@ exports.show = (req, res) => {
   .catch((err) => {
     return handleError(err)
   })
-};
+}
 
 /**
 * @api {post} /api/excused_absences/:id Create
@@ -73,7 +73,7 @@ exports.create = (req, res) => {
     return handleError(res, err)
   })
 
-};
+}
 
 /**
 * @api {put} /api/excused_absences/:id Update
@@ -85,28 +85,14 @@ exports.create = (req, res) => {
 * @apiError (500) UnknownException Could not update ExcusedAbsence model
 */
 exports.update = (req, res) => {
-
-  Post.findById(req.params.id, function (err, post) {
-    if (err) { return handleError(res, err); }
-    if(!post) { return res.send(404); }
-
-    // Only the post's author, a mentor, or an admin can edit the post
-    var userId = req.user._id;
-    User.findById(userId, function(err, user) {
-      if (err) { return handleError(res, err); }
-
-      if (userId.equals(post.author) || user.role === 'mentor' || user.role === 'admin'){
-        var updated = _.merge(post, req.body);
-        updated.save(function (err) {
-          if (err) { return handleError(res, err); }
-          return res.json(200, post);
-        });
-      } else {
-        return handleError(res, err);
-      }
-    });
-  });
-};
+  // TODO - isolate valid attributes depending on user role
+  // Admin - update STATUS, DATE(?), REVIEWER_NOTE, REVIEWED_BY (automatic)
+  // User - update REASON, DATE(?)
+  return ExcusedAbsence.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+  .then((response) => {
+      return res.status(200).send(response).end()
+  }).catch(next)
+}
 
 /**
 * @api {delete} /api/excused_absences/:id Delete
@@ -114,28 +100,13 @@ exports.update = (req, res) => {
 * @apiGroup Excused Absence
 * @apiDescription Deletes an ExcusedAbsence record
 * @apiPermission private
-* @apiSuccess {Model} root The updated Excused Absence model
-* @apiError (500) UnknownException Could not update ExcusedAbsence model
+* @apiSuccess {Model} root The destroyed ExcusedAbsence model
+* @apiError (500) UnknownException Could not destroy ExcusedAbsence model
 */
-exports.destroy = (req, res) => {
-  Post.findById(req.params.id, function (err, post) {
-    if(err) { return handleError(res, err); }
-    if(!post) { return res.send(404); }
-
-    // Only the post's author, a mentor, or an admin can delete the post
-    var userId = req.user._id;
-    User.findById(userId, function(err, user) {
-      if (err) { return handleError(res, err); }
-      if (userId.equals(post.author) || user.role === 'mentor' || user.role === 'admin'){
-        post.remove(function(err) {
-          if(err) { return handleError(res, err); }
-          return res.send(204);
-        });
-      } else {
-        return handleError(res, err);
-      }
-
-    });
-
-  });
-};
+exports.destroy = (req, res, next) => {
+  // TODO - ensure this is only deletable by the user who created the record, or an admin
+  return ExcusedAbsence.remove({ _id: req.params.id })
+  .then((response) => {
+      return res.status(200).send(response).end()
+  }).catch(next)
+}
