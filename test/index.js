44dfe1c5d8ee63b78385ff5e5d34601b41a2e321
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-03 16:02:35 -0400

    Integrated existing server tests into new coverage tool

--- test/index.js
+++ test/index.js
@@ -21,5 +21,16 @@ chai.use(require('chai-as-promised'));
 chai.use(require('chai-things'))
 
 // API tests
+require('../server/api/attendance/attendance.spec');
+require('../server/api/attendance/attendance.integration');
+require('../server/api/classyear/classyear.spec');
+require('../server/api/commit/commit.spec');
+require('../server/api/post/post.spec');
+require('../server/api/project/project.spec');
+require('../server/api/smallgroup/smallgroup.spec');
+require('../server/api/static/static.spec');
+require('../server/api/user/index.spec');
+require('../server/api/user/user.controller.spec');
+require('../server/api/user/user.integration');
+require('../server/api/user/user.model.spec');
 require('../server/api/user/user.spec');
-// require('../server/api/user/user.controller.spec');
