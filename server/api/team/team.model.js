493cb6956fcac49d405d76889b56b3ffc2bff61c
Dennis Chau <chaud@rpi.edu>
2018-10-16 17:15:53 -0400

    Added team model

---
+++ server/api/team/team.model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TeamSchema = new Schema({
+  title: String,
+  content: String,
+
+  title:
+  {
+    type: String,
+    index: true
+  },
+
+
+  userId: 
+  {
+    type : Schema.Types.ObjectId,
+    ref: 'User',
+    index: true
+  },
+  semesterId: 
+  {
+    type : Schema.Types.ObjectId,
+    ref: 'ClassYear',
+    index: true
+  },
+  projectId: 
+  {
+    type : Schema.Types.ObjectId,
+    ref: 'Project',
+    index: true
+  }
+},{ timestamps: true});
+
+module.exports = mongoose.model('Team', TeamSchema);
